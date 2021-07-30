b4f0b0ef946074dcff67528053bc7534adfdd40c
Aravidan Chockalingam <aravindc@sivapriya.org>
2021-07-30 10:34:10 +0100

    saving state

--- app/main.py
+++ app/main.py
@@ -1,21 +1,43 @@
+# from fastapi import FastAPI
+# from fastapi.middleware.cors import CORSMiddleware
+
+# from app.core.config import settings
+
+
+# def get_application():
+#     _app = FastAPI(title=settings.PROJECT_NAME)
+
+#     _app.add_middleware(
+#         CORSMiddleware,
+#         allow_origins=[str(origin) for origin in settings.BACKEND_CORS_ORIGINS],
+#         allow_credentials=True,
+#         allow_methods=["*"],
+#         allow_headers=["*"],
+#     )
+
+#     return _app
+
+
+# app = get_application()
+
+
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
+from tortoise.contrib.fastapi import register_tortoise
+from config.db import DB_CONFIG
+from config.settings import Settings
+import uvicorn
 
-from app.core.config import settings
+settings = Settings()
+app = FastAPI(title=settings.APP_NAME)
 
+print(DB_CONFIG)
 
-def get_application():
-    _app = FastAPI(title=settings.PROJECT_NAME)
-
-    _app.add_middleware(
-        CORSMiddleware,
-        allow_origins=[str(origin) for origin in settings.BACKEND_CORS_ORIGINS],
-        allow_credentials=True,
-        allow_methods=["*"],
-        allow_headers=["*"],
-    )
-
-    return _app
+register_tortoise(
+    app,
+    config=DB_CONFIG,
+    generate_schemas=False,
+)
 
 
-app = get_application()
+if __name__ == '__main__':
+    uvicorn.run(app, host=settings.HOST, port=settings.PORT)
